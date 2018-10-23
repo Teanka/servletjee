16574767417fee157d93671b3626daf6bfc1d922
Teanka <arolicka@gmail.com>
2018-10-23 22:01:13 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/Servlet14.java
@@ -0,0 +1,27 @@
+package pl.coderslab;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet14", urlPatterns = "/Servlet14")
+public class Servlet14 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    public void init() throws ServletException {
+        System.out.println("Zadanie 4");
+    }
+
+    public void destroy(){
+        System.out.println("destroy");
+    }
+}
